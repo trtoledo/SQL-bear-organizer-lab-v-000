a734d9e90eae988fb5cbe191d974f2609991ca6b
Tomas Toledo <trtoledo1977@gmail.com>
2018-06-27 00:19:51 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Yes", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Rowdy", 1, "Male", "Gray", "Yes", 21000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Tabitha", 4, "Female", "Black", "Yes", 181800);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Sergeant Brown", 2, "Male", "Brown", "Yes", 3000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Melissa", 3, "Female", "Brown", "Yes", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Grinch", 1, "Male", "Gray", "Yes", 21000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Wendy", 4, "Female", "BLack", "Yes", 181800);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("nill", 2, "Male", "Brown", "Yes", 3000000);
