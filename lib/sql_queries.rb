2de9721634b9e4ecf9a79f4ebf73968fe9bae432
Tomas Toledo <trtoledo1977@gmail.com>
2018-06-27 01:10:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,19 +7,19 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive == 'Yes'"
+  "SELECT name, age FROM bears WHERE alive == 'Yes' ORDER BY age DESC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT MAX(age) name, age FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT MIN(age) name, age FROM bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT COUNT(color) FROM bears WHERE color == 'Black';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
