b022b4dde8b5bd8646f78f65f4320f9566607795
Tomas Toledo <trtoledo1977@gmail.com>
2018-06-27 01:34:07 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,13 +6,3 @@ INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Me
 INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Grinch", 2, "M", "Black", "Grinchy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Wendy", 6, "F", "Blue", "naive", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES (null, 20, "M", "black", "aggressive", 0);
-
-
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
