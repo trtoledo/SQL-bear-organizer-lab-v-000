6f0c3f66f8b57bfd54da96a5cb948186d433726d
Tomas Toledo <trtoledo1977@gmail.com>
2018-06-27 00:29:38 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Yes", 10000);
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Rowdy", 1, "Male", "Gray", "Yes", 20000);
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Tabitha", 4, "Female", "Black", "Yes", 30000);
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Sergeant Brown", 2, "Male", "Brown", "Yes", 40000);
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Melissa", 3, "Female", "Brown", "Yes", 50000);
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Grinch", 1, "Male", "Gray", "Yes", 60000);
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Wendy", 4, "Female", "BLack", "Yes", 70000);
-INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("nil", 2, "Male", "Brown", "Yes", 80000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Calm", "Yes", 10000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Rowdy", 1, "Male", "Gray", "Nervous", "Yes", 20000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Tabitha", 4, "Female", "Black", "Calm", "Yes", 30000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Sergeant Brown", 2, "Male", "Brown", "Dangerous", "Yes", 40000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Melissa", 3, "Female", "Brown", "Nervous", "Yes", 50000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Grinch", 1, "Male", "Gray", "Calm", "Yes", 60000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Wendy", 4, "Female", "BLack", "Calm", "Yes", 70000);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("nil", 2, "Male", "Brown", "Calm", "Yes", 80000);
